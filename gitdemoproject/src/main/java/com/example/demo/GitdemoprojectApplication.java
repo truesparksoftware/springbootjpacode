a05908eea496d2d6cb74ef425c930f59962bd4bc
truesparksoftware@gmail.com <jeevanbarguje1@gmail.com>
2022-11-02 07:37:11 +0530

    added initial code

---
+++ gitdemoproject/src/main/java/com/example/demo/GitdemoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitdemoprojectApplication.class, args);
+	}
+
+}
